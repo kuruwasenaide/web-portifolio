f18370393ef1bf47491017336245db6738ae06bb
kaue <ykmoreira@gmail.com>
2025-02-28 15:40:55 -0300

    tirei o titulo animado

--- main.js
+++ main.js
@@ -206,39 +206,39 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // pasted moving title code
 
-function MovingTitle(writeText, interval, visibleLetters) {
-    var _instance = {};
+// function MovingTitle(writeText, interval, visibleLetters) {
+//     var _instance = {};
 
-    var _currId = 0;
-    var _numberOfLetters = writeText.length;
+//     var _currId = 0;
+//     var _numberOfLetters = writeText.length;
 
-    function updateTitle() {
-        _currId += 1;
-        if(_currId > _numberOfLetters - 1) {
-            _currId = 0; 
-        }
+//     function updateTitle() {
+//         _currId += 1;
+//         if(_currId > _numberOfLetters - 1) {
+//             _currId = 0; 
+//         }
 
-        var startId = _currId;
-        var endId = startId + visibleLetters;
-        var finalText;
-        if(endId < _numberOfLetters - 1) {
-            finalText = writeText.substring(startId, endId);
-        } else {
-            var cappedEndId = _numberOfLetters;
-            endId = endId - cappedEndId;
+//         var startId = _currId;
+//         var endId = startId + visibleLetters;
+//         var finalText;
+//         if(endId < _numberOfLetters - 1) {
+//             finalText = writeText.substring(startId, endId);
+//         } else {
+//             var cappedEndId = _numberOfLetters;
+//             endId = endId - cappedEndId;
 
-            finalText = writeText.substring(startId, cappedEndId) +     writeText.substring(0, endId);
-        }
+//             finalText = writeText.substring(startId, cappedEndId) +     writeText.substring(0, endId);
+//         }
 
-        document.title = finalText; 
-    }
+//         document.title = finalText; 
+//     }
 
-    _instance.init = function() {
-        setInterval(updateTitle, interval);
-    };
+//     _instance.init = function() {
+//         setInterval(updateTitle, interval);
+//     };
 
-    return _instance;
-}
+//     return _instance;
+// }
 
-var title = new MovingTitle("clean-code ", 300, 10);
-title.init();
+// var title = new MovingTitle("clean-code ", 300, 10);
+// title.init();
